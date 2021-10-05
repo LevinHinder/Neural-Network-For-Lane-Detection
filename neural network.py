b38a0369e068072e7df0d1579e1e484cf0d7581d
LevinHinder <88492367+LevinHinder@users.noreply.github.com>
2021-10-05 07:02:18 +0200

    Add files via upload

--- neural network.py
+++ neural network.py
@@ -1,11 +1,22 @@
 import numpy as np
 import os
 import cv2
-import pickle
 import copy
 import glob
 import json
 import dill
+import psutil
+
+
+# Print iterations progress
+def printProgressBar(iteration, total, prefix="", suffix="", length=100, fill="█", printEnd="\r"):
+    percent = ("{0:.1f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + "-" * (length - filledLength)
+    print(f"\r{prefix} |{bar}| {percent}% {suffix}", end=printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
 
 
 class Layer_Dense:
@@ -139,8 +150,10 @@ class Optimizer_Adam:
         bias_cache_corrected = layer.bias_cache / (1 - self.beta_2 ** (self.iterations + 1))
 
         # Update parameters
-        layer.weights -= self.current_learning_rate * weight_momentum_corrected / (np.sqrt(weight_cache_corrected) + self.epsilon)
-        layer.biases -= self.current_learning_rate * bias_momentum_corrected / (np.sqrt(bias_cache_corrected) + self.epsilon)
+        layer.weights -= self.current_learning_rate * weight_momentum_corrected / (
+                    np.sqrt(weight_cache_corrected) + self.epsilon)
+        layer.biases -= self.current_learning_rate * bias_momentum_corrected / (
+                    np.sqrt(bias_cache_corrected) + self.epsilon)
 
     def post_update_params(self):
         # Update iterations after any parameter updates
@@ -185,6 +198,31 @@ class Loss:
         # Reset variables for accumulated loss
         self.accumulated_sum = 0
         self.accumulated_count = 0
+
+
+class Loss_BinaryCrossentropy(Loss):
+
+    def forward(self, y_pred, y_true):
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # Calculate sample-wise loss
+        sample_losses = -(y_true * np.log(y_pred_clipped) + (1 - y_true) * np.log(1 - y_pred_clipped))
+        sample_losses = np.mean(sample_losses, axis=-1)
+
+        # Return losses
+        return sample_losses
+
+    def backward(self, dvalues, y_true):
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        clipped_dvalues = np.clip(dvalues, 1e-7, 1 - 1e-7)
+
+        # Calculate gradient
+        self.dinputs = -(y_true / clipped_dvalues - (1 - y_true) / (1 - clipped_dvalues)) / len(dvalues[0])
+        # Normalize gradient
+        self.dinputs = self.dinputs / len(dvalues)
 
 
 class Loss_UnbalancedSegmentation(Loss):
@@ -353,12 +391,6 @@ class Model:
 
     def train(self, epochs=1, batch_size=None, print_every=1):
 
-        # ==========================================================================================================
-        # F:\Curvelanes\valid\images\000a706ea929755e6bf6583ff2ce4a81.jpg
-        check = cv2.imread(r"D:\Curvelanes\train\images\000a9e2091e2902d988ffdd87258fc29.jpg")
-        check = self.image_preprocess(check)
-        # ==========================================================================================================
-
         # Main training loop
         for epoch in range(1, epochs + 1):
             train_steps = 1
@@ -417,11 +449,6 @@ class Model:
                         self.optimizer.update_params(layer)
                     self.optimizer.post_update_params()
 
-                    # ====================================================================================================
-                    if not self.index % 1000:
-                        model.save(fr"F:\Curvelanes\{epoch}.{train_steps}.model")
-                    # ====================================================================================================
-
                     # Print a summary
                     if not train_steps % print_every or filename == last_file:
                         print(f"step: {train_steps}, acc: {accuracy:}, loss: {loss:.3f} (data_loss: {data_loss:.3f}, "
@@ -429,14 +456,6 @@ class Model:
 
                         # Save statistical information
                         self.save_stat(key="train", accuracy=accuracy, data_loss=data_loss, reg_loss=reg_loss)
-
-                        # ==========================================================================================================
-                        predict = self.forward(check, training=False)
-                        predict = self.output_layer_activation.predictions(predict)
-                        predict *= 255
-                        predict = predict.reshape(self.res_out[1], self.res_out[0])
-                        cv2.imwrite(fr"C:\Users\Levin\Downloads\New folder (2)\{epoch}.{train_steps}.png", predict)
-                        # ==========================================================================================================
 
                     self.index += 1
                     train_steps += 1
@@ -511,21 +530,33 @@ class Model:
         self.save_stat(key="valid", accuracy=valid_accuracy, data_loss=valid_data_loss, reg_loss=0)
 
     def predict(self, path):
-        # ==========================================================================================================
-        import time
-        start_time = time.time()
-        # ==========================================================================================================
+
+        # Check whether path is valid
+        if not os.path.exists(path):
+            print("Invalid path!\n")
+            return
+
         # Create video object
         video_input = cv2.VideoCapture(path)
+        # Get filename of video
+        index = np.maximum(path.rfind("\\"), path.rfind("/"))
+        filename = path[index + 1:path.rfind(".")]
         # Get width and height of video
         width = int(video_input.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(video_input.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        # Get number of frames
+        length = int(video_input.get(cv2.CAP_PROP_FRAME_COUNT))
         # Define the codec and create VideoWriter object
-        fourcc = cv2.VideoWriter_fourcc(*"MP4V")
-        video_output = cv2.VideoWriter(os.path.join(os.getcwd(), "output.mp4"), fourcc, 30, (width, height))
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+        video_output = cv2.VideoWriter(os.path.join(os.getcwd(), filename + "_output.mp4"), fourcc, 30, (width, height))
         # Create arrays
         X = []
         frames = []
+
+        max_array_size = int(psutil.virtual_memory().available / height / width / 5)
+
+        process = 0
+        printProgressBar(process, length + 1, prefix="Progress:", suffix="Complete", length=50)
 
         while True:
             # Read the video frame by frame
@@ -538,36 +569,42 @@ class Model:
             # Save original frame
             frames.append(frame)
 
-        # Convert the data to proper numpy arrays
-        X = np.array(X)
-        frames = np.array(frames)
+            if not len(X) % max_array_size or process + len(X) == length:
+                # Convert the data to proper numpy arrays
+                X = np.array(X)
+                frames = np.array(frames)
 
-        # Pass the data through the network
-        output = self.forward(X, training=False)
-        predictions = self.output_layer_activation.predictions(output)
-        # Reshape predictions to 2d array
-        predictions = predictions.reshape(predictions.shape[0], self.res_out[1], self.res_out[0])
-        # Add array filled with zeros that was cut away from the network
-        fill = np.zeros((predictions.shape[0], int(self.res_out[1] / (1 - self.poi) * self.poi), self.res_out[0]))
-        predictions = np.append(fill, predictions, axis=1)
+                # Pass the data through the network
+                output = self.forward(X, training=False)
+                predictions = self.layers[-1].predictions(output)
+                # Reshape predictions to 2d array
+                predictions = predictions.reshape(predictions.shape[0], self.res_out[1], self.res_out[0])
+                # Add array filled with zeros that was cut away from the network
+                fill = np.zeros(
+                    (predictions.shape[0], int(self.res_out[1] / (1 - self.poi) * self.poi), self.res_out[0]))
+                predictions = np.append(fill, predictions, axis=1)
 
-        # Iterate over all frames
-        for i in range(len(predictions)):
-            # Resize network output to original video resolution
-            predict = cv2.resize(predictions[i], (width, height))
-            # Mark predicted pixels in video red
-            frames[i, predict >= 0.5] = [0, 0, 255]
-            # Write frame as video
-            video_output.write(frames[i])
+                # Iterate over all frames
+                for i in range(len(predictions)):
+                    # Resize network output to original video resolution
+                    predict = cv2.resize(predictions[i], (width, height))
+                    # Mark predicted pixels in video red
+                    frames[i, predict >= 0.5] = [0, 0, 255]
+                    # Write frame as video
+                    video_output.write(frames[i])
+                    process += 1
+                    printProgressBar(process, length + 1, prefix="Progress:", suffix="Complete", length=50)
+
+                # Clear arrays
+                X = []
+                frames = []
+
+                max_array_size = int(psutil.virtual_memory().available / height / width / 5)
 
         # Release everything when job is finished
         video_input.release()
         video_output.release()
-        # ==========================================================================================================
-        end_time = time.time()
-        time_lapsed = end_time - start_time
-        print(time_lapsed)
-        # ==========================================================================================================
+        printProgressBar(length + 1, length + 1, prefix="Progress:", suffix="Complete", length=50)
 
     def forward(self, X, training):
 
@@ -625,7 +662,7 @@ class Model:
         # Open file in the binary-read mode
         with open(path, "rb") as f:
             # Load the model
-            model = pickle.load(f)
+            model = dill.load(f)
 
         return model
 
@@ -659,15 +696,13 @@ model.set(loss=Loss_UnbalancedSegmentation(),
 # Finalize the model
 model.finalize()
 
-# Save the model
-model.save(r"D:\Curvelanes\lane_detection.model")
-
 # Train the model
 model.train(epochs=100000, batch_size=4, print_every=3)
 
 # Save the model
 model.save(r"F:\Curvelanes\lane_detection.model")
 
+# Load the model
 model = Model.load(r"D:\Curvelanes\1.2.model")
 
 # Use the model to predict data
